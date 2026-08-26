db39346389ab231764c0142072107f27638f9723
Andrea$21 <a.nieri89@gmail.com>
2026-08-26 15:02:55 +0200

    Update PWA cache for modular app

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const CACHE='kz-carbweather-v1.4';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='kz-carbweather-v1.4-git';
+const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./ui1.html','./ui2.html','./ui3.html','./data.js','./app-core.js','./app-telemetry.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).then(r=>{
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
-  }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
 });
