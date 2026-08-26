bad7ef551dea077f5bfc8c2c836bd932529cfdd1
Andrea$21 <a.nieri89@gmail.com>
2026-08-26 16:19:21 +0200

    Refresh PWA cache for single-file runtime

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='kz-carbweather-v1.4-git';
-const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./ui1.html','./ui2.html','./ui3.html','./data.js','./app-core.js','./app-telemetry.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='kz-carbweather-v1.7-trackdb';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
   e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
