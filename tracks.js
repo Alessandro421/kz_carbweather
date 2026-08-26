93bddaf8205b55bce4604e35744d25534e83c077
Andrea$21 <a.nieri89@gmail.com>
2026-08-26 16:18:19 +0200

    Add curated KZ track database

---
+++ tracks.js
@@ -0,0 +1,53 @@
+/* Curated kart track database used before generic geocoding. Coordinates are track-level, not city centroids. */
+const TRACK_DATABASE = [
+  {
+    id:'pista-del-mare',
+    name:'Pista del Mare',
+    city:'Cecina',
+    region:'Toscana',
+    country:'Italia',
+    latitude:43.284492,
+    longitude:10.522563,
+    aliases:['pista del mare','cecína','cecina','kartodromo cecina','paduletto','pista del mare cecina']
+  },
+  {
+    id:'south-garda-karting',
+    name:'South Garda Karting',
+    city:'Lonato del Garda',
+    region:'Lombardia',
+    country:'Italia',
+    latitude:45.425096,
+    longitude:10.490904,
+    aliases:['south garda','south garda karting','lonato','lonato del garda','kartodromo lonato']
+  },
+  {
+    id:'franciacorta-karting-track',
+    name:'Franciacorta Karting Track',
+    city:'Castrezzato',
+    region:'Lombardia',
+    country:'Italia',
+    latitude:45.514170,
+    longitude:10.000616,
+    aliases:['franciacorta','franciacorta karting','franciacorta karting track','castrezzato']
+  },
+  {
+    id:'kartodromo-val-vibrata',
+    name:'Kartodromo Val Vibrata',
+    city:'Sant’Egidio alla Vibrata',
+    region:'Abruzzo',
+    country:'Italia',
+    latitude:42.810000,
+    longitude:13.686667,
+    aliases:['val vibrata','kartodromo val vibrata','sant egidio alla vibrata','santegidio alla vibrata','faraone']
+  },
+  {
+    id:'pista-rioveggio',
+    name:'Pista Rioveggio',
+    city:'Rioveggio',
+    region:'Emilia-Romagna',
+    country:'Italia',
+    latitude:44.287109,
+    longitude:11.208015,
+    aliases:['rioveggio','pista rioveggio','kartodromo rioveggio','karting rioveggio']
+  }
+];
