880a1df63832e5da40a6fb12c0a349b440c16211
Andrea$21 <a.nieri89@gmail.com>
2026-09-11 08:14:35 +0200

    Scope service worker cache cleanup to KZ caches

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+const CACHE_PREFIX='kz-carbweather-';
 const CACHE='kz-carbweather-v2.1-appshell';
 const ASSETS=[
   './',
@@ -22,7 +23,7 @@ self.addEventListener('activate',e=>{
       }
     }
     const keys=await caches.keys();
-    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    await Promise.all(keys.filter(k=>k.startsWith(CACHE_PREFIX)&&k!==CACHE).map(k=>caches.delete(k)));
     await self.clients.claim();
   })());
 });
